21fa52cf6ecb56646971807f8f849ff7600f7b30
panduramadhan2 <panduramadhan2@gmail.com>
2024-03-28 08:51:29 +0700

    first custom component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,18 +1,6 @@
 import products from "@/assets/data/products";
-import Colors from "@/src/constants/Colors";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
-
-// const product = products[1];
-
-const ProductListItem = ({ product }) => {
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
-  );
-};
+import ProductListItem from "@/src/components/ProductListItem";
+import { View } from "react-native";
 
 export default function MenuScreen() {
   return (
@@ -22,20 +10,3 @@ export default function MenuScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  title: { fontSize: 18, fontWeight: "600", marginVertical: 10 },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-});
