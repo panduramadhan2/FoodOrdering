553477bd91c1578337b4cd49fbaed5ac9ea9f506
panduramadhan2 <panduramadhan2@gmail.com>
2024-04-12 00:33:14 +0700

    checkpoint for modal

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,9 +1,48 @@
-import { Stack } from "expo-router";
+// import Colors from "@/src/constants/Colors";
+import Colors from "@/src/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function MenuStack() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        // headerTitle: () => (
+        //   <View style={styles.headerContainer}>
+        //     <Text style={styles.headerText}>Menu</Text>
+        //   </View>
+        // ),
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="shopping-cart"
+                  size={25}
+                  color={Colors.light.tint}
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Menu" }} />
     </Stack>
   );
 }
+
+// const styles = StyleSheet.create({
+//   headerContainer: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginLeft: "-7%",
+//   },
+//   headerText: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+// });
---
+++ src/app/cart.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Platform } from "react-native";
+
+const CartScreen = () => {
+  return (
+    <View>
+      <Text>CartScreen</Text>
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+    </View>
+  );
+};
+
+export default CartScreen;
