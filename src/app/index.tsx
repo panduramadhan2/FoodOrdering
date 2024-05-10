e8ac020bfa4718e383c9af6af80a8d90fe84acb8
panduramadhan2 <panduramadhan2@gmail.com>
2024-05-10 09:44:40 +0700

    checkpoint supabase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import React from "react";
 import Button from "../components/Button";
-import { Link } from "expo-router";
+import { Link, Redirect } from "expo-router";
+import { useAuth } from "../providers/AuthProvider";
 
 const index = () => {
+  const { session, loading } = useAuth();
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+  if (!session) {
+    return <Redirect href={"/sign-in"} />;
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
       <Link href={"/(user)"} asChild>
