d8c93b96bd9edc9b4a90aee5d0e08e05e8fed248
panduramadhan2 <panduramadhan2@gmail.com>
2024-06-11 16:50:36 +0700

    investigasi loading indikator first page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,30 @@
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, Alert } from "react-native";
 import React from "react";
 import Button from "../components/Button";
 import { Link, Redirect } from "expo-router";
 import { useAuth } from "../providers/AuthProvider";
+import { supabase } from "../lib/supabase";
+import * as SecureStore from "expo-secure-store";
 
 const index = () => {
   const { session, loading } = useAuth();
 
-  if (loading) {
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      Alert.alert(error.message);
+    } else {
+      // Clear the stored access token
+      await SecureStore.deleteItemAsync("access_token");
+      console.log("logout");
+      Alert.alert("Signed out successfully");
+    }
+  }
+//////////////////////////////////////
+  if (!loading) {
     return <ActivityIndicator />;
   }
+  //problem di loading indicator
   if (!session) {
     return <Redirect href={"/sign-in"} />;
   }
@@ -22,9 +37,10 @@ const index = () => {
       <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
       </Link>
-      <Link href={"/sign-in"} asChild>
+      {/* <Link href={"/sign-in"} asChild>
         <Button text="Sign in" />
-      </Link>
+      </Link> */}
+      <Button onPress={signOut} text="Sign out" />
     </View>
   );
 };
